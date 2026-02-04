7f9ff791b225d6c9959acca18944d03f936c7f51
nuwandara1dbts-ctrl <nuwandara1dbts@gmail.com>
2026-02-04 15:30:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://collection.cloudinary.com/dny6s0aul/43f68d25d67bf9750b061136546f5e6e", // Music streaming URL
+        musicUrl: "https://collection.cloudinary.com/dny6s0aul/a59dd6d1f1c26fb0957863c46771ace3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
